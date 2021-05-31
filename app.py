07ef54539fc95300543fc9efaa1570a1f6bd8795
Fabio Augusto da Silva <fabaugsilv@gmail.com>
2021-05-31 18:51:25 -0300

    Alterando texto da pagina

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório Pipeline DevOps - ENTREGA"
+    return "Laboratório Pipeline DevOps"
 
 if __name__ == '__main__':
     app.run(debug=True)
